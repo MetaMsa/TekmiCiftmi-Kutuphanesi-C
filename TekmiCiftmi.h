463f56eebb9f21316e239d5c8a61fbd992a07176
Serhat <mserhataslan@hotmail.com>
2023-11-12 18:18:36 +0300

    a

---
+++ TekmiCiftmi.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <tchar.h>
+#include <conio.h>
+
+using namespace std;
+
+char tek_cift(int a)
+{
+ if(a%2==0)
+ {
+  return 'c';
+ }
+
+ if(a%2==1)
+ {
+  return 't';
+ }
+}
